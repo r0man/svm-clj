c0e47f23ef43c4c919cf6a5ba277593f71c9ddd0
Roman <roman@burningswell.com>
2011-10-05 22:35:13 +0200

    Added format-libsvm-line.

--- src/svm/data.clj
+++ src/svm/data.clj
@@ -1,5 +1,10 @@
 (ns svm.data
-  (:use [clojure.string :only (split)]))
+  (:use [clojure.string :only (join split)]))
+
+(defn format-libsvm-line
+  "Format data as a LibSVM text line."
+  [[label data]]
+  (str label " " (->> (sort data) (map (partial join ":")) (join " "))))
 
 (defn parse-libsvm-line
   "Parse a LibSVM formatted text line."
@@ -11,5 +16,5 @@
         (assoc data
           (Integer/parseInt index)
           (Double/parseDouble value)))
-      {} (map #(split % #":") data))]))
+      (sorted-map) (map #(split % #":") data))]))
 
