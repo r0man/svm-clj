2fbf463284be9cd86420f22f14150d2110aee7b2
Roman <roman@burningswell.com>
2011-10-05 22:36:53 +0200

    Removed partial.

--- src/svm/data.clj
+++ src/svm/data.clj
@@ -4,7 +4,7 @@
 (defn format-libsvm-line
   "Format data as a LibSVM text line."
   [[label data]]
-  (str label " " (->> (sort data) (map (partial join ":")) (join " "))))
+  (str label " " (->> (sort data) (map #(join ":" %)) (join " "))))
 
 (defn parse-libsvm-line
   "Parse a LibSVM formatted text line."
