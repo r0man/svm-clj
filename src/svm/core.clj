b9152a89ff6f538de4ac445890110437e5432a2f
Roman <roman@burningswell.com>
2012-01-10 21:12:03 +0100

    Merged namespaces into core.

--- src/svm/core.clj
+++ src/svm/core.clj
@@ -1,12 +1,8 @@
 (ns svm.core
-  (:import [libsvm svm_parameter]))
-
-(def svm-types
-  {:c-svc svm_parameter/C_SVC
-   :epsilon-svr svm_parameter/EPSILON_SVR
-   :nu-svc svm_parameter/NU_SVC
-   :nu-svr svm_parameter/NU_SVR
-   :one-class svm_parameter/ONE_CLASS})
+  (:refer-clojure :exclude (replace))
+  (:import [libsvm svm_node svm_parameter svm_problem svm])
+  (:use [clojure.java.io :only (file reader writer)]
+        [clojure.string :only (join replace split)]))
 
 (def kernel-types
   {:linear svm_parameter/LINEAR
@@ -15,4 +11,107 @@
    :rbf svm_parameter/RBF
    :sigmoid svm_parameter/SIGMOID})
 
+(def svm-types
+  {:c-svc svm_parameter/C_SVC
+   :epsilon-svr svm_parameter/EPSILON_SVR
+   :nu-svc svm_parameter/NU_SVC
+   :nu-svr svm_parameter/NU_SVR
+   :one-class svm_parameter/ONE_CLASS})
 
+(def default-params
+  {:C 1
+   :cache-size 100
+   :coef0 0
+   :degree 3
+   :eps 1e-3
+   :gamma 0
+   :kernel-type (:rbf kernel-types)
+   :nr-weight 0
+   :nu 0.5
+   :p 0.1
+   :probability 0
+   :shrinking 1
+   :svm-type (:c-svc svm-types)
+   :weight (double-array 0)
+   :weight-label (int-array 0)})
+
+(defn- max-features [dataset]
+  (apply max (mapcat (comp keys last) dataset)))
+
+(defn format-libsvm-line
+  "Format data as a LibSVM text line."
+  [[label data]]
+  (str label " " (->> (sort data) (map #(join ":" %)) (join " "))))
+
+(defn make-node
+  "Make a LibSVM node."
+  [index value]
+  (let [node (svm_node.)]
+    (set! (. node index) index)
+    (set! (. node value) value)
+    node))
+
+(defn make-nodes
+  "Make a seq of LibSVM nodes."
+  [[label data]] (map #(apply make-node %) data))
+
+(defn make-params [dataset & {:as options}]
+  (let [params (svm_parameter.)]
+    (doseq [[key val] (merge default-params options)]
+      (clojure.lang.Reflector/setInstanceField params (replace (name key) "-" "_") val))
+    (set! (.gamma params) (double (/ 1 (max-features dataset))))
+    params))
+
+(defn make-problem
+  "Make a LibSVM problem."
+  [dataset]
+  (let [problem (svm_problem.)]
+    (set! (. problem l) (count dataset))
+    (set! (. problem y) (double-array (map first dataset)))
+    (set! (. problem x) (into-array (map into-array (map make-nodes dataset))))
+    problem))
+
+(defn parse-libsvm-line
+  "Parse a LibSVM formatted text line."
+  [line]
+  (let [[label & data] (split line #"\s+")]
+    [(Integer/parseInt label)
+     (reduce
+      (fn [data [index value]]
+        (assoc data
+          (Integer/parseInt index)
+          (Double/parseDouble value)))
+      (sorted-map) (map #(split % #":") data))]))
+
+(defn read-dataset
+  "Read the dataset from url."
+  [url] (map parse-libsvm-line (line-seq (reader url))))
+
+(defn write-dataset
+  "Write the dataset to filename."
+  [dataset filename]
+  (let [file (file filename)]
+    (.mkdirs (.getParentFile file))
+    (spit file (join "\n" (map format-libsvm-line dataset)))
+    (str file)))
+
+(defn train-model
+  "Train a model with dataset according to options."
+  [dataset & options]
+  (let [problem (make-problem dataset)
+        params (apply make-params dataset options)]
+    (svm/svm_check_parameter problem params)
+    (svm/svm_train problem params)))
+
+(defn save-model
+  "Save the model to filename."
+  [model filename]
+  (svm/svm_save_model filename model)
+  filename)
+
+(defn predict
+  "Predict the label of the `feature` with `model`."
+  [model feature]
+  (->> (make-nodes [nil feature])
+       (into-array)
+       (svm/svm_predict model)))
--- test/svm/test/core.clj
+++ test/svm/test/core.clj
@@ -1,3 +1,114 @@
 (ns svm.test.core
-  (:use clojure.test
+  (:import [libsvm svm_node svm_model svm_parameter svm_problem])
+  (:use [clojure.java.io :only (reader)]
+        clojure.java.shell
+        clojure.test
         svm.core))
+
+(def example-dataset (read-dataset "test-resources/heart_scale"))
+
+(def example-model (train-model example-dataset))
+
+(def example-data
+  [-1 {1 0.166667, 2 1.0, 3 1.0, 4 -0.132075, 5 -0.69863, 6 -1.0, 7 -1.0, 8 0.175573, 9 -1.0, 10 -0.870968, 12 -1.0, 13 0.5}])
+
+(def example-line
+  "-1 1:0.166667 2:1 3:1 4:-0.132075 5:-0.69863 6:-1 7:-1 8:0.175573 9:-1 10:-0.870968 12:-1 13:0.5")
+
+(deftest test-format-libsvm-line
+  (let [line (format-libsvm-line example-data)]
+    (is (= "-1 1:0.166667 2:1.0 3:1.0 4:-0.132075 5:-0.69863 6:-1.0 7:-1.0 8:0.175573 9:-1.0 10:-0.870968 12:-1.0 13:0.5" line))
+    (is (= example-data (parse-libsvm-line line)))))
+
+(deftest test-heart-scale
+  (is (= 0 (:exit (sh "svm-train" "test-resources/heart_scale" "tmp/heart_scale.model"))))
+  (is (= 0 (:exit (sh "svm-predict" "test-resources/heart_scale" "tmp/heart_scale.model" "tmp/output"))))
+  (is (= (map #(Double/parseDouble %1) (line-seq (reader "tmp/output")))
+         (map #(predict example-model %1) (map last example-dataset)))))
+
+(deftest test-make-problem
+  (let [problem (make-problem example-dataset)]
+    (is (instance? svm_problem problem))))
+
+(deftest test-make-node
+  (let [node (make-node 1 0.166667)]
+    (is (instance? svm_node node))
+    (is (= 1 (. node index)))
+    (is (= 0.166667 (. node value)))))
+
+(deftest test-make-nodes
+  (let [nodes (make-nodes (first example-dataset))]
+    (is (= 12 (count nodes)))))
+
+(deftest test-make-params
+  (let [dataset (read-dataset "test-resources/heart_scale")]
+    (let [params (make-params dataset)]
+      (is (instance? svm_parameter params))
+      (is (= svm_parameter/C_SVC (.svm_type params)))
+      (is (= svm_parameter/RBF (.kernel_type params)))
+      (is (= 3 (.degree params)))
+      (is (= 0.07692307692307691 (.gamma params)))
+      (is (= 0.0 (.coef0 params)))
+      (is (= 0.5 (.nu params)))
+      (is (= 100.0 (.cache_size params)))
+      (is (= 1.0 (.C params)))
+      (is (= 1e-3 (.eps params)))
+      (is (= 0.1 (.p params)))
+      (is (= 1 (.shrinking params)))
+      (is (= 0 (.probability params)))
+      (is (= 0 (.nr_weight params)))
+      (is (= 0 (count (.weight params))))
+      (is (= 0 (count (.weight_label params)))))
+    (let [params (make-params dataset :C 2)]
+      (is (instance? svm_parameter params))
+      (is (= svm_parameter/C_SVC (.svm_type params)))
+      (is (= svm_parameter/RBF (.kernel_type params)))
+      (is (= 3 (.degree params)))
+      (is (= 0.07692307692307691 (.gamma params)))
+      (is (= 0.0 (.coef0 params)))
+      (is (= 0.5 (.nu params)))
+      (is (= 100.0 (.cache_size params)))
+      (is (= 2.0 (.C params)))
+      (is (= 1e-3 (.eps params)))
+      (is (= 0.1 (.p params)))
+      (is (= 1 (.shrinking params)))
+      (is (= 0 (.probability params)))
+      (is (= 0 (.nr_weight params)))
+      (is (= 0 (count (.weight params))))
+      (is (= 0 (count (.weight_label params)))))))
+
+(deftest test-parse-libsvm-line
+  (let [[label data] (parse-libsvm-line example-line)]
+    (is (= -1 label))
+    (is (= 0.166667 (get data 1)))
+    (is (nil? (get data 11)))
+    (is (= 0.5 (get data 13)))))
+
+(deftest test-predict
+  (are [index label]
+    (is (= label (predict example-model (last (nth example-dataset index)))))
+    0 1.0
+    1 -1.0
+    2 -1.0
+    3 1.0))
+
+(deftest test-read-dataset
+  (let [dataset (read-dataset "test-resources/heart_scale")]
+    (is (= 270 (count dataset)))
+    (is (every? #(= 2 (count %1)) dataset))
+    (is (every? #(number? (first %1)) dataset))
+    (is (every? #(map? (second %1)) dataset))))
+
+(deftest test-save-model
+  (is (= "tmp/heart_scale.model"
+         (save-model example-model "tmp/heart_scale.model"))))
+
+(deftest test-train-model
+  (let [model (train-model example-dataset)]
+    (is (instance? svm_model model))))
+
+(deftest test-write-dataset
+  (let [dataset (read-dataset "test-resources/heart_scale")
+        filename (write-dataset dataset "tmp/heart_scale")]
+    (is (= "tmp/heart_scale" filename))
+    (is (= dataset (read-dataset filename)))))
