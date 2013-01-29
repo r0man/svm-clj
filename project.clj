2be7ffd9db051fb2b5b525e8ffa35acff6bc8966
Roman Scherer <roman.scherer@burningswell.com>
2013-01-29 20:27:22 +0100

    Added :license to project.clj.

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
             :name "Clojure LibSVM"
             :web-src-dir "https://github.com/r0man/svm-clj/blob/"
             :web-home "https://r0man.github.com/svm-clj/"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Clojure Library for LibSVM."
   :url "https://github.com/r0man/svm-clj"
   :dependencies [[org.clojure/clojure "1.4.0"]
