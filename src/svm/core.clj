aee56f34079ab75c490ce9d101c77b11558d757f
ToBeReplaced <ToBeReplaced@gmail.com>
2013-03-22 10:39:56 -0400

    Added sparse-map to core with corresponding test.

--- src/svm/core.clj
+++ src/svm/core.clj
@@ -64,6 +64,19 @@
     (set! (.gamma params) (/ 1.0 (max-features dataset)))
     params))
 
+(defn sparse-map
+  "Returns a sparse map of features from seq.  The map will be suitable
+  as the second entry in a dataset vector.  seq should have length
+  equal to the total number of features in the dataset and set empty
+  values to nil.
+
+  Ex. (sparse-map [5 nil 7]) -> {1 5, 3 7}"
+  [seq]
+  (->> seq
+       (map vector (rest (range)))
+       (keep (fn [[i v]] (when v [i v])))
+       (into (sorted-map))))
+
 (defn make-problem
   "Make a LibSVM problem from `dataset`."
   [dataset]
