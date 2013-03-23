a0ff3da7bc9542c127bd7b75c04d5aadd51c9178
Roman Scherer <roman.scherer@burningswell.com>
2013-03-23 10:13:47 +0100

    Started 0.1.3-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject svm-clj "0.1.2"
+(defproject svm-clj "0.1.3-SNAPSHOT"
   :author "Roman Scherer"
   :autodoc {:copyright "Copyright (c) 2011 Roman Scherer"
             :name "Clojure LibSVM"
